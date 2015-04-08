db0bd05077b0c2a7a2ce8399954333c0e035390d
fbourbour <fredbourbour@comcast.net>
2015-04-07 23:52:43 -0500

    SD Card Test

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include "task.h"
 #include "queue.h"
 #include "croutine.h"
+#include "ff.h"
 
 #include <system.h>
 #include <stdio.h>
@@ -21,6 +22,16 @@
 #include <clock.h>
 #include <command.h>
 #include <string.h>
+#include "ff.h"
+#include "diskio.h"
+
+static int Test1();
+
+/////////////////////////////////////////////////////////////////////
+
+
+
+
 /* Application includes */
 
 /*Init system config*/
@@ -29,6 +40,7 @@
 
 
 static void initTask(void * params);
+static int ReadSDCard();
 
 static void lowLevelHardwareInit()
 {
@@ -54,12 +66,20 @@ static void initializeBackupRegisters()
 
 int main( void )
 {
-    lowLevelHardwareInit();
+     lowLevelHardwareInit();
+
+    SysTick_Config(3000);
+        //Test1();
+       disk_initialize(0);
+       ReadSDCard(); //debug remove
+
+       return (1);
+
     xTaskCreate(initTask, NULL, systemDEFAULT_STACK_SIZE, NULL, systemPRIORITY_INIT, NULL);
     vTaskStartScheduler();
     
     
-    /* Will only reach here if there is insufficient heap available to start
+   /* Will only reach here if there is insufficient heap available to start
     the scheduler. */
     return 0;
 }
@@ -76,13 +96,13 @@ void initTask(void * params)
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
     
     GPIO_Init(GPIOA, &GPIO_InitStructure);
-    /* End test heartbeat initialization */
+    /*  End test heartbeat initialization */
     
     /* Begin actual init */
     int i;
     
     I2C_Initialize();
-    SPI_Initialize();
+    //SPI_Initialize();
     
     vUartStartTask();
     vConsolePrintf("\r\n\r\n==================== BOOT ====================\r\n");
@@ -144,23 +164,47 @@ void initTask(void * params)
 //TODO: Port    startStorageDriverTask();
     
     vConsolePrintf("Init finished!\r\n");
+    vConsolePrintf("Testing SD Write!\r\n");
     
+
+
 #if 1
     uint8_t i2cbuffer[1000];
     uint8_t spitxbuffer[100];
     uint8_t spirxbuffer[100];
 #endif
     
+    vConsolePrintf("Testing SPI transfer...");
+    //test pattern to send to spi port via DMA
+         spitxbuffer[0] =0x01;
+         spitxbuffer[1] =0x02;
+         spitxbuffer[2] =0x04;
+         spitxbuffer[3] =0x06;
+         spitxbuffer[4] =0x08;
+         spitxbuffer[5] =0x01;
+
+      //SPI1_Transfer(spitxbuffer, spirxbuffer,6, portMAX_DELAY);
+     vConsolePrintf("Spi Transfer Done\r\n");
+
+
     for(;;)
     {
         GPIO_SetBits(GPIOA, GPIO_Pin_5);
-        vTaskDelay(500);
+        vTaskDelay(500); //
         GPIO_ResetBits(GPIOA, GPIO_Pin_5);
         vTaskDelay(500);
-        
+
+        vConsolePrintf("Testing SPI transfer...");
+        SPI1_Transfer(spitxbuffer, spirxbuffer,5, portMAX_DELAY);
+        vConsolePrintf("Spi Transfer Done\r\n");
+
+        continue; //debug. remove later
+
 #if 1
         /* I2C test */
         
+        vConsolePrintf("Debug1!\r\n");
+
         bzero(i2cbuffer, 100);
         I2CSYS_TakeMutex(portMAX_DELAY);
         i2cbuffer[0] = 10;
@@ -204,9 +248,64 @@ void initTask(void * params)
         SPI1_TakeMutex(portMAX_DELAY);
         SPI1_Transfer(spitxbuffer, spirxbuffer, 100, portMAX_DELAY);
         SPI1_ReleaseMutex();
-        vConsolePrintf("Done\r\n");
+        vConsolePrintf("Done\r\n"); //
 #endif
     }
+}
+
+int ReadSDCard()
+{
+	UINT bw;
+
+	/** Read a text file and display it */
+
+	FATFS FatFs;   /* Work area (file system object) for logical drive */
+    FIL fil,fil1;       /* File object */
+    //char line[82]; /* Line buffer */
+    FRESULT fr;    /* FatFs return code */
+    //FIL fsrc;      // File objects
+    BYTE buffer[512];    /* was 4096 File copy buffer */
+    UINT br;
+
+
+	     /* Register work area to the default drive **/
+	    f_mount(&FatFs, "", 0);
+	    /* Open a text file */
+
+	    if (f_open(&fil1, "file.txt", FA_WRITE | FA_OPEN_ALWAYS) == FR_OK)
+	    {	// Open existing or create new file
+	    		if (f_lseek(&fil1, f_size(&fil1)) == FR_OK)
+	    			{
+	    				char *text2 = "This is a new line, appended to existing file!\r\n";
+	    				f_write(&fil1, text2, strlen(text2), &bw); // Write data to the file
+	    				if (bw == strlen(text2))
+	    				{ //we wrote the entire string
+	    					printf("Appended:%d\n", (int)f_size(&fil1));
+	    				}
+
+
+	    			}
+	    	f_close(&fil1);// Close the file
+	    }
+
+	    char buff[50];
+	    strcpy(buff,"file.txt");
+	    fr = f_open(&fil, "Bourbour.txt", FA_READ);
+	    //fr = f_open(&fil,buff, FA_READ);
+	    if (fr) return (int)fr;
+
+	    for (;;)
+	    		{
+	            	fr = f_read(&fil, buffer, sizeof buffer, &br);  /* Read a chunk of source file */
+	            	//fr = f_read(&fsrc, buffer,1, &br);  /* Read a chunk of source file */
+	            	if (fr || br == 0)
+	            		break; /* error or eof */
+	            	 printf(buffer);
+	           	 }
+	    /* Close the file */
+	    f_close(&fil);
+
+	    return 0;
 }
 
 void vApplicationStackOverflowHook()
@@ -218,3 +317,21 @@ void vApplicationMallocFailedHook()
 {
   for(;;) {}
 }
+
+
+int Test1 ()
+{
+    int rc;
+    DWORD buff[512];  /* 2048 byte working buffer  **/
+
+      /****  Check function/compatibility of the physical drive #0 **/
+    rc = test_diskio(0, 1, buff, sizeof buff);
+    if (rc) {
+    	 vConsolePrintf("Sorry the function/compatibility test failed.\nFatFs will not work on this disk driver.\n");
+    } else {
+    	vConsolePrintf("Congratulations! The disk I/O layer works well.\n");
+    }
+
+    return rc;
+}
+
